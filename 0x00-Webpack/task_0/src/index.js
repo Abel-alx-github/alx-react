e79983883d431d01dcbb666e685c70f4821e59ad
Abel-alx-github <abeltibeso@gmail.com>
2024-06-24 22:06:00 +0300

    Create and run Webpack using a basic installation.

--- 0x00-Webpack/task_0/src/index.js
+++ 0x00-Webpack/task_0/src/index.js
@@ -1,8 +1,5 @@
-// a script that adds a p element with some text to the body of the HTML document
-
 import $ from 'jquery';
 
-$(document).ready( function () {
-    const para = "<p>Holberton Dashboard</p> <p>Dashboard data for the students</p> <p>Copyright - Holberton School</p>"
-    $("body").append(para); 
-})
+$('body').append('<p>Holberton Dashboard</p>');
+$('body').append('<p>Dashboard data for the students</p>');
+$('body').append('<p>Copyright - Holberton School</p>');
