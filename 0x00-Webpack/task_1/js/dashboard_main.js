197a04fa4e9435d2662c09adca5c324bb69ed90e
Abel-alx-github <abeltibeso@gmail.com>
2024-06-24 23:48:16 +0300

    Webpack with a config file.

--- 0x00-Webpack/task_1/js/dashboard_main.js
+++ 0x00-Webpack/task_1/js/dashboard_main.js
@@ -1,13 +1,12 @@
 import $ from 'jquery'
 import _ from 'lodash'
 
-const para1 = "<p>Holberton Dashboard</p>";
-const para2 = "<p>Dashboard data for the students</p>";
-const btn ="<button>Click here to get started</button>"
-const para3 = "<p id='count'></p>"
-const para4 = "<p>Copyright - Holberton School</p>" 
+$('body').append('<p>Holberton Dashboard</p>');
+$('body').append('<p>Dashboard data for the students</p>');
+$('body').append('<button>Click here to get started</button>');
+$('body').append('<p id="count"></p>');
+$('body').append('<p>Copyright - Holberton School</p>');
 
-$('body').append(para1, para2, btn, para3, para4);
 
 let count = 0
 function updateCounter() {
