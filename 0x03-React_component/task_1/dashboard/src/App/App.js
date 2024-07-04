ee32b9b00d70f749f7357a7fa33f5f406cbfe88e
Abel-alx-github <abeltibeso@gmail.com>
2024-07-04 14:36:25 +0300

    App class component

--- 0x03-React_component/task_1/dashboard/src/App/App.js
+++ 0x03-React_component/task_1/dashboard/src/App/App.js
@@ -2,10 +2,12 @@ import React, { Component, Fragment } from 'react';
 import Header from '../Header/Header';
 import Login from '../Login/Login';
 import Footer from '../Footer/Footer';
+
 import Notifications from '../Notifications/Notifications';
 import CourseList from '../CourseList/CourseList';
-import PropTypes from 'prop-types';
 import { getLatestNotification } from '../utils/utils';
+
+import PropTypes from 'prop-types';
 
 class App extends Component {
   constructor(props) {
@@ -15,9 +17,11 @@ class App extends Component {
   componentDidMount() {
     window.addEventListener('keydown', this.handleLogout);
   }
+
   componentWillUnmount() {
     window.removeEventListener('keydown', this.handleLogout);
   }
+
   handleLogout(e) {
     if (e.ctrlKey && e.key === 'h') {
       e.preventDefault();
@@ -25,12 +29,14 @@ class App extends Component {
       this.props.logOut();
     }
   }
+
   render() {
     const listCourses = [
       { id: 1, name: 'ES6', credit: 60 },
       { id: 2, name: 'Webpack', credit: 20 },
       { id: 3, name: 'React', credit: 40 },
     ];
+
     const listNotifications = [
       { id: 1, type: 'default', value: 'New course available' },
       { id: 2, type: 'urgent', value: 'New resume available' },
@@ -57,5 +63,6 @@ App.propTypes = {
   isLoggedIn: PropTypes.bool,
   logOut: PropTypes.func,
 };
+
 
 export default App;
