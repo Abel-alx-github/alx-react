bcebe390af7a2d269d6ccc29c6d3a1e3a8c3aca4
Abel-alx-github <abeltibeso@gmail.com>
2024-07-18 21:08:50 +0300

    react immutable

--- 0x06-React_Immutable/8-seq.js
+++ 0x06-React_Immutable/8-seq.js
@@ -1,21 +1,16 @@
 import { Seq } from 'immutable';
 
-
 export default function printBestStudents(object) {
   const seq = Seq(object);
-
 
   const filtered = seq.filter((student) => {
     student.firstName.charAt(0).toUpperCase();
     return student.score > 70;
-
   });
-
 
   function capFirstLetter(string) {
     return string.charAt(0).toUpperCase() + string.slice(1);
   }
-
 
   const JSObject = filtered.toJS();
 
